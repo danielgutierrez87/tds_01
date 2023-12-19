308098c512fceb58d47c9a9e0a58cb150c2f4a0d
Daniel Gutierrez <146106786+danielgutierrez87@users.noreply.github.com>
2023-12-19 23:17:03 +0000

    novos exercicios

---
+++ daniel/gato.java
@@ -0,0 +1,10 @@
+
+
+Gato(String nome, String raca, String cor, Integer idade){ // metodo construtor, o que define o cão !
+    this.onomatopeia = "*som de gato*";
+    this.nome = nome;
+    this. raca = raca;
+    this.cor = cor;
+    this.idade = idade;
+  }
+
