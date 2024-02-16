cc59fc3c0a365d9b3c7a7f6dfbe143c1253082b2
Daniel Gutierrez <146106786+danielgutierrez87@users.noreply.github.com>
2024-02-16 00:12:47 +0000

    novos exercicios

---
+++ daniel/ex002.java
@@ -0,0 +1,10 @@
+public class ex002 {
+    public static void main(String[] args) {
+     
+     String texto = "java";
+     
+     for (int i = texto.length()-1; i >= 0; i--) {
+      System.out.print(texto.charAt(i));
+     }
+    }
+   }
