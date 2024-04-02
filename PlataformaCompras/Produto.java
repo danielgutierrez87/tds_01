7a5b2393ed86e1d22bfb38376b67277d916d54bf
Daniel Gutierrez <daniell.mx@gmail.com>
2024-04-02 16:28:05 -0300

    finalizado

--- PlataformaCompras/Produto.java
+++ PlataformaCompras/Produto.java
@@ -1,33 +1,33 @@
 package PlataformaCompras;
 
 public class Produto {
-<<<<<<< HEAD
+    <<<<<<< HEAD
+        
+        private String nome;
+        private Double preco;
+    =======
     
-    private String nome;
-    private Double preco;
-=======
-
-    private String nomeProd;
-    private float precoProd;
->>>>>>> 9fd79aac3b5e5e55f6ecef8f9130b662bd9f6dcf
-    private String descricao;
-
-    public String getNome() {
-        return this.nome;
+        private String nomeProd;
+        private float precoProd;
+    >>>>>>> 9fd79aac3b5e5e55f6ecef8f9130b662bd9f6dcf
+        private String descricao;
+    
+        public String getNome() {
+            return this.nome;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public Double getPreco() {
+            return this.preco;
+        }
+        public void setPreco(Double preco) {
+            this.preco = preco;
+        }
+        public String getDescricao() {
+            return this.descricao;
+        }
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
     }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public Double getPreco() {
-        return this.preco;
-    }
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-    public String getDescricao() {
-        return this.descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-}
