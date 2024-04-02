ad320b30d215493d041f00ee6fc34a5f6db0fe14
Daniel Gutierrez <146106786+danielgutierrez87@users.noreply.github.com>
2024-04-02 20:22:08 -0300

    novo exercicio

--- PlataformaCompras/Pedido.java
+++ PlataformaCompras/Pedido.java
@@ -1,5 +1,4 @@
 package PlataformaCompras;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
--- PlataformaCompras/Plataforma.java
+++ PlataformaCompras/Plataforma.java
@@ -1,52 +1,21 @@
 package PlataformaCompras;
-
 import java.util.Scanner;
 
 public class Plataforma {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
         Pedido pedido = new Pedido();
 
-<<<<<<< HEAD
         String continuar = "";
-        
-=======
-        LocalDateTime data = LocalDateTime.now();
-        DateTimeFormatter formatData = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        pedido.setData(data.format(formatData));
-
-        System.out.println("Login");
-
-        System.out.print("Nome: ");
-        usuario.setNome(input.nextLine());
-
-        System.out.print("Email: ");
-        usuario.setEmail(input.nextLine());
-
-        System.out.print("Senha: ");
-        usuario.setSenha(input.nextInt());
-        System.out.println();
-
-        pedido.setCliente(usuario);
-
-        String continuar;
-
->>>>>>> 9fd79aac3b5e5e55f6ecef8f9130b662bd9f6dcf
         do {
             Produto produto = new Produto();
             System.out.print("Produto: ");
             produto.setNome(scan.nextLine());
 
             System.out.print("Preço: ");
-<<<<<<< HEAD
             produto.setPreco(scan.nextDouble());
             scan.nextLine();
-=======
-            produto.setPrecoProd(input.nextFloat());
-            input.nextLine();
->>>>>>> 9fd79aac3b5e5e55f6ecef8f9130b662bd9f6dcf
 
             System.out.print("Descrição: ");
             produto.setDescricao(scan.nextLine());
